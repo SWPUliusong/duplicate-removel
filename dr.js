988bee899734de7172daab4284d8cec181ed0474
liusong <swpuliusong@foxmail.com>
2017-03-27 00:37:37 +0800

    version

--- dr.js
+++ dr.js
@@ -3,22 +3,23 @@
 const program = require("commander")
 const del = require("del")  // 删除文件和文件夹的模块
 
+const package = require("./package.json")
 const filter = require("./filter")
 const rm = require("./remove")  // 备用删除方案
 
 // 定义命令行参数和帮助文档
 program
-    .version('1.0.0')
-    .description('  去除文件夹下重复的文件')
+    .version(package.version)
+    .description(`  ${package.description}`)
     .option('-i, --ignore <dirname>', '忽略的文件夹(以,连接多个)')    // <>dirname的参数
     .option('-s, --short', '路径深度更小或名字更短的优先保留')
     .option('-a, --allow-empty', '允许空文件夹存在 (默认删除空文件夹)')
     .on('--help', function () {
         console.log('  Example:')
         console.log('')
-        console.log('   $ dr -s')
-        console.log('   $ dr -i node_modules,.git')
-        console.log('   $ dr -i node_modules,.git -s')
+        console.log('    $ dr -s')
+        console.log('    $ dr -i node_modules,.git')
+        console.log('    $ dr -i node_modules,.git -s')
         console.log('')
     })
 
